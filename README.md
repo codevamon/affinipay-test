aa2c9e3440a3deaae5ad684bd315f528f5aee0bf
Paul Stein <pstein@affinipay.com>
2023-12-08 15:39:30 -0600

    Expanding API functionality

---
+++ README.md
@@ -0,0 +1,50 @@
+# README #
+
+## Info
+
+This repository contains the source needed to execute a container that hosts a simple api. Please follow the instructions to get the api running on your local system and then 
+implement automated test cases for the running api. Feel free to use whichever language/framework you are most comfortable with. 
+
+## Installing dependencies 
+
+Please install the following dependencies:
+
+1) Docker
+   1) https://docs.docker.com/engine/install/
+2) Git
+   1) https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+
+## Cloning Exercise Repo
+```aidl
+git clone https://github.com/affinipay/candidate-exercise.git
+```
+
+## Building and Running Container
+To Build:
+```aidl
+docker build -t excercize:1.0
+```
+To Run:
+```aidl
+docker run --name demo -it -p 5000:5000 excercize:1.0
+```
+You should now be able to access the api through this URL
+
+```aidl
+http://127.0.0.1:5000/dogs
+```
+
+## API SPEC
+
+The api provides an endpoint to create, veiw and delete dogs from the system. The following operations are allowed
+
+End users are allowed to create dogs by POSTing to http://127.0.0.1:5000/dogs, they request payload should contain the following fields
+
+```aidl
+{
+    "breed": <string>,
+    "age":   <int>,
+    "name":  <string>
+}
+```
+The system assigns a unique ID to each dog created which will be visible in the POST response. This ID can be used to view and delete dogs from the system through the URL http://127.0.0.1:5000/dogs/<dog_id>
--- src/api.py
+++ src/api.py
@@ -2,26 +2,54 @@ from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
+data_store = {}
 
-@app.route('/', methods=['GET'])
-def hello_world():
-	user_data = {
-        "user_id": '1234',
-        "name": "Testy McTesterton",
-        "email": "foo@bar.com"
-    }
-	return jsonify(user_data), 200
+@app.route('/dogs/<dog_id>', methods = ['GET', 'DELETE', 'PUT', 'PATCH', 'POST'])
+def dogs_slash_id(dog_id):
+    if request.method == 'GET':
+        if dog_id in data_store.keys():
+            return jsonify(data_store[dog_id]), 200
+        else:
+            return "Doggo Not Found", 404
+    if request.method == 'DELETE':
+        if dog_id in data_store.keys():
+            data = data_store.pop(dog_id)
+            return jsonify(data), 200
+        else:
+            return "Doggo Not Found", 404
+    else:
+        return "Woof! Method Not Allowed", 405
 
-@app.route("/<user_id>", methods=['GET'])
-def get_user(user_id):
-    user_data = {
-        "user_id": user_id,
-        "name": "Testy McTesterton",
-        "email": "foo@bar.com"
-    }
-    return jsonify(user_data), 200
+@app.route("/dogs/", methods = ['GET', 'DELETE', 'PUT', 'PATCH', 'POST'])
+def dogs():
+    if request.method == 'GET':
+        return("All the dogs", 200)
+    if request.method == 'POST':
+        request_data = request.get_json()
+        if check_payload(request_data):
+            new_index = str(len(data_store.keys()) + 1)
+            request_data['id']=new_index
+            data_store[new_index]=request_data
+            return jsonify(request_data), 200
+        else:
+            return "Invalid Doggy Payload", 422
+    else:
+        return("Woof! Method Not Allowed", 405)
+
+@app.route('/', methods = ['GET', 'DELETE', 'PUT', 'PATCH', 'POST'])
+def slash():
+    if request.method == 'POST':
+	    return('Ru Roh Raggy, This should\'nt have succeeded', 200)
+    else:
+        return("Woof! Method Not Allowed", 405)
 
 
+
+def check_payload(payload):
+    if("name" in payload.keys()) and ("breed" in payload.keys()) and ("age" in payload.keys()):
+        return  (type(payload['name']) == str) and (type(payload['age']) == int) and (type(payload['breed']) == str)
+    else:
+        return false
 
 
 if __name__ == "__main__":
